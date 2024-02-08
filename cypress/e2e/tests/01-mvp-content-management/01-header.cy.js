b8b2751a2abf17f09cd5fe454972d29481d58c01
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-02-08 16:34:50 +0100

    header update categories

--- cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
@@ -17,15 +17,30 @@ describe('Check the Header', function () {
   context('Header categories', function () {
     it('I have access to main products categories from a top menu', function () {
       cy.visit(`${config.baseUrl}`);
-      cy.get('[href="https://test-bachmann-confiserie.arcmedia.ch/de/saison"]')
+      cy.get('.menu-dropdown-icon').contains('Saison&Spezialitäten')
         .should('be.visible')
         .click();
       cy.get('.menu > :nth-child(2) > :nth-child(2) > :nth-child(1)')
         .should('be.visible')
         .click();
+      cy.get(':nth-child(2) > :nth-child(3) > [href="https://test-bachmann-confiserie.arcmedia.ch/de/lieferservice"]')
+        .should('be.visible')
+        .click();
+      cy.get(':nth-child(2) > :nth-child(4) > [href="https://test-bachmann-confiserie.arcmedia.ch/de/schokolade-1"]')
+        .should('be.visible')
+        .click();
+      cy.get('[href="https://test-bachmann-confiserie.arcmedia.ch/de/kundengeschenke"]')
+        .should('be.visible')
+        .click();
+      cy.get('.menu > :nth-child(2) > :nth-child(6) > :nth-child(1)')
+        .should('be.visible')
+        .click();
+      cy.get('[href="https://test-bachmann-confiserie.arcmedia.ch/bachmann/unternehmen/uebersicht"]')
+        .should('be.visible')
+        .click();
     });
 
-    it('I have access to the cart and see a mini cart in a header menu as a guest', function () {
+    it.skip('I have access to the cart and see a mini cart in a header menu as a guest', function () {
       cy.visit(`${config.baseUrl}`);
     });
   });
