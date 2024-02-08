07cd93779b9021fe5bbc29e2922bbba4e6bd444e
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-02-08 14:16:19 +0100

    updating the header

--- cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
@@ -6,17 +6,27 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Check the Header', function () {
 
-  context('because reCaptcha is enabled, we are unable to log in to my account', function () {
-    it('I have access to my account link in a header menu', function () {
-      cy.visit(`${config.baseUrl}`);
-    });
+  beforeEach('before each test', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('#btn-cookie-allow')
+      .should('be.visible')
+      .click();
+    cy.wait(2000);
   });
 
   context('Header categories', function () {
     it('I have access to main products categories from a top menu', function () {
+      cy.visit(`${config.baseUrl}`);
+      // cy.get('.menu-dropdown-icon.dropdown a[href="https://test-bachmann-confiserie.arcmedia.ch/de/saison"]')
+      //   .should('be.visible')
+      //   .click();
+      cy.get('.menu > :nth-child(2) > :nth-child(2) > :nth-child(1)')
+        .should('be.visible')
+        .click();
     });
 
     it('I have access to the cart and see a mini cart in a header menu as a guest', function () {
+      cy.visit(`${config.baseUrl}`);
     });
   });
 });
