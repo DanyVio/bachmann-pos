64b1bca36d0f91e2dc4b00b06a42551101ed76c3
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-02-08 11:23:25 +0100

    updates on cofig file

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: "enytfo",
+  projectId: "",
   viewportWidth: 1920,
   viewportHeight: 1280,
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: "https://arcmedia:arcmedia123@test-bachmann-confiserie.arcmedia.ch/",
+    baseUrl: "https://arcmedia:arcmedia123@test-pos.confiserie.arcmedia.ch/",
     viewportWidth: 1920,
     viewportHeight: 1280,
     specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
