3225ea3dbed639626783b2f658cb2b1058157b08
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-02-08 11:38:15 +0100

    updates in catalog and categories

--- cypress/e2e/tests/03-catalog-product-and-categories/04-doofinder-search-engine.cy.js
+++ cypress/e2e/tests/03-catalog-product-and-categories/04-doofinder-search-engine.cy.js
@@ -6,27 +6,8 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Doofinder', function () {
 
-  beforeEach('before each test', function () {
-    cy.visit(`${config.baseUrl}`);
-    cy.get('#btn-cookie-allow')
-      .should('be.visible')
-      .click();
-    cy.wait(3000);
-  });
-
   it('doofinder', function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('#search')
-      .type('macarons');
-    cy.get(':nth-child(2) > .title')
-      .should('be.visible')
-      .click();
-    cy.get('.mst-searchautocomplete__show-all > a > span')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(1) > .mst-search__index-title > [href="https://test-bachmann-confiserie.arcmedia.ch/de/apero"]')
-      .should('be.visible')
-      .click();
   });
 });
 
