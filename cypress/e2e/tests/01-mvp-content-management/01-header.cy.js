28e408dfaa77d5cee9acfd95ab83666e46be75a7
Danyvio <daniel.viorel@arcmedia.ch>
2024-02-28 11:50:32 +0100

    log in as an employee user

--- cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
@@ -8,38 +8,21 @@ describe('Check the Header', function () {
 
   beforeEach('before each test', function () {
     cy.visit(`${config.baseUrl}`);
+    cy.get('.amgdprcookie-button.-allow.-save')
+      .should('be.visible')
+      .click();
+    cy.wait(3000);
     cy.get('#btn-cookie-allow')
       .should('be.visible')
       .click();
-    cy.wait(2000);
   });
 
-  context('Header categories', function () {
-    it('I have access to main products categories from a top menu', function () {
+  context('Log In', function () {
+    it('Log in as a employee user', function () {
       cy.visit(`${config.baseUrl}`);
-      cy.get('.menu-dropdown-icon').contains('Saison&Spezialitäten')
-        .should('be.visible')
-        .click();
-      cy.get('.menu > :nth-child(2) > :nth-child(2) > :nth-child(1)')
-        .should('be.visible')
-        .click();
-      cy.get(':nth-child(2) > :nth-child(3) > [href="https://test-bachmann-confiserie.arcmedia.ch/de/lieferservice"]')
-        .should('be.visible')
-        .click();
-      cy.get(':nth-child(2) > :nth-child(4) > [href="https://test-bachmann-confiserie.arcmedia.ch/de/schokolade-1"]')
-        .should('be.visible')
-        .click();
-      cy.get('[href="https://test-bachmann-confiserie.arcmedia.ch/de/kundengeschenke"]')
-        .should('be.visible')
-        .click();
-      cy.get('.menu > :nth-child(2) > :nth-child(6) > :nth-child(1)')
-        .should('be.visible')
-        .click();
-      cy.get('[href="https://test-bachmann-confiserie.arcmedia.ch/bachmann/unternehmen/uebersicht"]')
-        .should('be.visible')
-        .click();
-    });
-
+      cy.get('#employee_number')
+        .type('1234');
+      });
     it.skip('I have access to the cart and see a mini cart in a header menu as a guest', function () {
       cy.visit(`${config.baseUrl}`);
     });
