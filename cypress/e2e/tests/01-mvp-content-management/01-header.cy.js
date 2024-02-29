261e29ccbdc29737c09e0490d19c47cfefe80355
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-02-29 13:00:02 +0100

    updating pop-up cookies

--- cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
@@ -8,26 +8,25 @@ describe('Header', function () {
 
   beforeEach('before each test', function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('#btn-cookie-allow')
+    cy.get('.allow')
       .should('be.visible')
       .click();
     cy.wait(3000);
-    cy.get('.amgdprcookie-button.-allow.-save')
+    cy.get('#btn-cookie-allow')
       .should('be.visible')
-      .click();
+      .click(); 
   });
 
-  context('Log In', function () {
-    it('Log in as a employee user', function () {
-      cy.visit(`${config.baseUrl}`);
-      cy.get('#employee_number')
-        .type('1234');
-      cy.get('#submit-button')
-        .should('be.visible')
-        .click();
-      });
-    it.skip('I have access to the cart and see a mini cart in a header menu as a guest', function () {
-      cy.visit(`${config.baseUrl}`);
+  
+  it('Log in as a employee user', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('#employee_number')
+      .type('1234');
+    cy.get('#submit-button')
+      .should('be.visible')
+      .click();
     });
+  it.skip('I have access to the cart and see a mini cart in a header menu as a guest', function () {
+    cy.visit(`${config.baseUrl}`);
   });
 });
