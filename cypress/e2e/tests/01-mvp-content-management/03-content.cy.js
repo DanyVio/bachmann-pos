4142cfa31f21918a346ba296381f9fafe7c74ee1
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-03 13:00:01 +0200

    update content homepage

--- cypress/e2e/tests/01-mvp-content-management/03-content.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/03-content.cy.js
@@ -6,7 +6,43 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Content page', function () {
 
+  beforeEach('cookie banner', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.get('#employee_number')
+      .type('1234');
+    cy.get('#submit-button')
+      .should('be.visible')
+      .click();
+  });
+
   it('components home page', function () {
     cy.visit(`${config.baseUrl}`);
+    cy.get(':nth-child(3) > .widget > .home-category-promotion-container > .home-category-promotion-wrapper > :nth-child(1) > a > img')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('bottom')
+    cy.get('.flex > img')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(4) > .widget > .home-category-promotion-container > .home-category-promotion-wrapper > :nth-child(1) > a > img')
+      .should('be.visible')
+      .click();
+    cy.get('.flex > img')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('center')
+    // cy.get('.home-season-promotion-item-left > a')
+    //   .should('be.visible')
+    //   .click();
+    cy.scrollTo('bottom')
+    cy.get(':nth-child(1) > .product-item-info > .product-image > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo')
+      .should('be.visible')
+      .click();
+    cy.get('.flex > img')
+      .should('be.visible')
+      .click();
   });
 });
