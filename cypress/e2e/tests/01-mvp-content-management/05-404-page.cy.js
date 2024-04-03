670ba43f906efec5955c0d2d126560d0bbbb8a03
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-03 13:02:25 +0200

    update 404 page

--- cypress/e2e/tests/01-mvp-content-management/05-404-page.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/05-404-page.cy.js
@@ -6,6 +6,18 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('404 page', function () {
 
+  beforeEach('cookie banner', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.get('#employee_number')
+      .type('1234');
+    cy.get('#submit-button')
+      .should('be.visible')
+      .click();
+  });
+
   it('404 page', function () {
     cy.visit(`${config.baseUrl}/cuatrocientoscuatro`, { failOnStatusCode: false });
   });
