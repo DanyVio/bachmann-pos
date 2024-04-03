748c5edd30aae9aad40dd5606d6ef56c39c6d92b
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-03 15:15:11 +0200

    update doofinder search

--- cypress/e2e/tests/02-catalog-product-and-categories/04-doofinder-search-engine.cy.js
+++ cypress/e2e/tests/02-catalog-product-and-categories/04-doofinder-search-engine.cy.js
@@ -6,8 +6,25 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Doofinder', function () {
 
+  beforeEach('cookie banner', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.get('#employee_number')
+      .type('1234');
+    cy.get('#submit-button')
+      .should('be.visible')
+      .click();
+  });
+
   it('doofinder', function () {
     cy.visit(`${config.baseUrl}`);
+    cy.get('#search')
+      .type('torten');
+    cy.get(':nth-child(4) > .flex-nowrap > .w-full > .text-gray-900')
+      .should('be.visible')
+      .click({ force: true});
   });
 });
 
