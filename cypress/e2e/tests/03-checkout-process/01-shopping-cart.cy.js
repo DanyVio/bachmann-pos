42493cba640095456e047d9ceaf0b536c98ffc1f
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-03 16:51:29 +0200

    update shopping cart and shipping method

--- cypress/e2e/tests/03-checkout-process/01-shopping-cart.cy.js
+++ cypress/e2e/tests/03-checkout-process/01-shopping-cart.cy.js
@@ -4,48 +4,35 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 });
 
-describe('Shopping Cart as a guest', function () {
+describe('Cart', function () {
 
-  // beforeEach('before each test', function () {
-  //   cy.visit(`${config.baseUrl}`);
-  //   cy.wait(3000);
-  //   cy.get('.-allow')
-  //     .should('be.visible')
-  //     .click();
-  //   cy.get('#btn-cookie-allow')
-  //     .should('be.visible')
-  //     .click();
-  // });
-
-  it('shopping cart as a guest', function () {
+  beforeEach('cookie banner', function () {
     cy.visit(`${config.baseUrl}`);
-    // cy.get('#employee_number')
-    //     .type('1234');
-    // cy.get('#submit-button')
-    //   .should('be.visible')
-    //   .click();
-    // cy.get('.menu > :nth-child(2) > :nth-child(6) > :nth-child(1)')
-    //   .should('be.visible')
-    //   .click();
-    cy.get(':nth-child(3) > [href="https://test-bachmann-confiserie.arcmedia.ch/de/torten/fototorten"]')
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.get('#employee_number')
+      .type('1234');
+    cy.get('#submit-button')
+      .should('be.visible')
+      .click();
+  });
+
+  it('shopping cart', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.w-full > .md-top-menu-items > .nav-587 > [href="https://test-pos.confiserie.arcmedia.ch/pos/saison"]')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(2) > :nth-child(1) > [href="https://test-pos.confiserie.arcmedia.ch/pos/spezielles/spezialitaten"]')
       .should('be.visible')
       .click();
     cy.get('.level0.active > .o-list > :nth-child(1) > a')
       .should('be.visible')
       .click();
-    // cy.get('.-allow')
-    //   .should('be.visible')
-    //   .click();
-    // cy.get('#btn-cookie-allow')
-    //   .should('be.visible')
-    //   .click();
-    cy.get(':nth-child(1) > .product-item-info > .d-mode-grid > .product-item-link')
+    cy.get(':nth-child(1) > .item > .product-image > .product')
       .should('be.visible')
       .click();
-    cy.get('#input-box-options_14829_file > .input-file-content > .file-input-trigger')
-      .should('be.visible')
-      .click();
-    cy.get('#product-addtocart-button')
+    cy.get('.qty > .plus')
       .should('be.visible')
       .click();
   });
